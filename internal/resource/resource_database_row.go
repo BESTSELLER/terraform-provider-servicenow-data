c0f9287fa3af503594bf1a1aacab4c7b30ced21e
Andrei Predoiu <andrei.predoiu@bestseller.com>
2022-09-20 14:31:52 +0200

    table row schema

--- internal/resource/resource_database_row.go
+++ internal/resource/resource_database_row.go
@@ -10,7 +10,7 @@ const databaseRowResourceName = "sn_application"
 // TODO
 func databaseRowResource() *schema.Resource {
 	return &schema.Resource{
-		Schema:         nil,
+		Schema:         RowSchema,
 		SchemaVersion:  1,
 		StateUpgraders: nil,
 		CreateContext:  nil,
@@ -32,4 +32,51 @@ func databaseRowResource() *schema.Resource {
 		Description:   "A row in a SN table",
 		UseJSONNumber: false,
 	}
+}
+
+var RowSchema = map[string]*schema.Schema{
+	"sys_id": {
+		Description: "The unique id of the row",
+		Type:        schema.TypeString,
+		Required:    false,
+		Optional:    false,
+		Computed:    true},
+	"sys_updated_by": {
+		Description: "User that made the last update",
+		Type:        schema.TypeString,
+		Required:    false,
+		Optional:    false,
+		Computed:    true},
+	"sys_created_by": {
+		Description: "Account that created the row",
+		Type:        schema.TypeString,
+		Required:    false,
+		Optional:    false,
+		Computed:    true},
+	"sys_created_on": {
+		Description: "Creation Time",
+		Type:        schema.TypeString,
+		Required:    false,
+		Optional:    false,
+		Computed:    true},
+	"sys_updated_on": {
+		Description: "Last update Time",
+		Type:        schema.TypeString,
+		Required:    false,
+		Optional:    false,
+		Computed:    true},
+	"simple_columns": {
+		Description: "Custom columns that are not references",
+		Type:        schema.TypeMap,
+		Elem: &schema.Schema{
+			Type: schema.TypeString}},
+	"reference_columns": {
+		Description: "Custom columns that are not references",
+		Type:        schema.TypeMap,
+		Elem: &schema.Schema{
+			Type: schema.TypeMap,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			}},
+	},
 }
