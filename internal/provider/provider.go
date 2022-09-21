c2d671deac87618ce11cb6333eb3eebc3b900fc7
Andrei Predoiu <andrei.predoiu@bestseller.com>
2022-09-21 15:37:54 +0200

    provider cleanup

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,6 +1,9 @@
 package provider
 
 import (
+	"context"
+	"github.com/BESTSELLER/terraform-provider-servicenow-data/internal/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
@@ -26,8 +29,51 @@ func ServiceNowDataProvider() *schema.Provider {
 				Description: "The URL to the SN table using basic auth"},
 		},
 
-		ResourcesMap:   resources,
-		DataSourcesMap: dataSources,
+		ResourcesMap:         resources,
+		DataSourcesMap:       dataSources,
+		ConfigureContextFunc: providerConfigure,
 	}
 	return p
+}
+
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var host, username, password *string
+	var diags diag.Diagnostics
+
+	hVal, ok := d.GetOk("sn_api_user")
+	if ok {
+		tempHost := hVal.(string)
+		username = &tempHost
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "sn_api_user - Missing required config",
+		})
+	}
+	hVal, ok = d.GetOk("sn_api_pass")
+	if ok {
+		tempHost := hVal.(string)
+		password = &tempHost
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "sn_api_pass - Missing required config",
+		})
+	}
+	hVal, ok = d.GetOk("sn_api_url")
+	if ok {
+		tempHost := hVal.(string)
+		host = &tempHost
+	} else {
+		diags = append(diags, diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  "sn_api_url - Missing required config",
+		})
+	}
+
+	// Warning or errors can be collected in a slice type
+
+	c := client.NewClient(*host, *username, *password)
+
+	return c, diags
 }
